0065a92b5013fdc7ffa3af83f3cbe73c0d16e49d
golfyplayz <146030843+golfyplayz@users.noreply.github.com>
2024-03-13 13:10:47 -0500

    minor changes

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 8040;
 
 app.get('/', (req, res, next) => {
   res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-})
+  console.log(`Server started on port ${PORT}`);
+});
 
 module.exports = app;
