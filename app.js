0f84b6d38f8b7a1361a77d823336d27ec73c3a2f
golfyplayz <146030843+golfyplayz@users.noreply.github.com>
2024-03-14 08:50:59 -0500

    ekwej;r

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { initDB } = require('./db');
+
 const app = express();
+app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 8040;
+const db = initDB();
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!');
+app.get("/users", (req, res) => {
+    db.all("SELECT * FROM users", [], (err, rows) => {
+        if (err) {
+            res.status(500).json({"error": err.message});
+        } else {
+            res.json({users: rows})
+        }
+    });
 });
 
-app.get('/test', (req, res, next) => {
-  res.send('Hello World! You added a new route!');
+app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    db.all("SELECT * FROM users where id is (?)", [id], (err, rows) => {
+        if (err) {
+            res.status(500).json({"error": err.message});
+        } else if (rows.length === 0) {
+            res.json({user: {}})
+        } else {
+            res.json({user: rows[0]})
+        }
+    })
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.post("/users", (req, res) => {
+    const { user: { username, password} } = req.body;
+    const insertStmt = "INSERT INTO users(username,password) VALUES (?,?)";
+    db.run(insertStmt, [username, password], function(err, result) {
+        if (err) {
+            res.status(500).json({ "error": err.message });
+        } else {
+            res.json({
+                id: this.lastID,
+                username,
+                password
+            })
+        }
+    })
 });
 
-module.exports = app;
+app.listen(4000, () => console.log("Simple server running on http://localhost:4000"))
